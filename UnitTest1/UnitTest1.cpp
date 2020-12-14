df5ed585f70299b8924f0557eac189a14e7886e3
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-12-14 15:19:07 +0200

    Initial commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = ProcessTXT("../t.txt");
+			Assert::AreEqual(t, 8);
+		}
+	};
+}
